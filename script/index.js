b4bf91bad2363176e7f713260344cda9bd5913f7
Al2com <acomenge@iesfuentesanluis.org>
2022-06-22 21:49:23 +0200

    restaurando

--- script/index.js
+++ script/index.js
@@ -1,4 +1,7 @@
-//elemento
+//cuando el usuariuo haga Click en el boton menu en el header
+//agragar la clase al menu-open al body
+//abrir menu
+//elemento, 
 const button = document.querySelector('button[type="button"]');
 //evento
 button.addEventListener('click', openMenu);
@@ -6,5 +9,15 @@ button.addEventListener('click', openMenu);
 //ejecución
 function openMenu(event) {
   document.body.classList.add('menu-open');
-  
 }
+//cerrar menu
+
+//elemento
+const closebutton = document.querySelector('menu button[type="button"]');
+//evento
+closebutton.addEventListener('click',closenMenu)
+
+//ejecuciónfunction openMenu(event) {
+ function closenMenu(event) {
+   document.body.classList.remove('menu-open');
+ }
